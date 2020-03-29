d77e86aa49d2fe6b3843eba8270cecba3cfa1b70
DanielCaoSilva <47014112+DanielCaoSilva@users.noreply.github.com>
2020-03-29 09:39:01 -0700

    Update MapsActivityCurrentPlace.java

--- app/src/main/java/com/example/currentplacedetailsonmap/MapsActivityCurrentPlace.java
+++ app/src/main/java/com/example/currentplacedetailsonmap/MapsActivityCurrentPlace.java
@@ -1,11 +1,15 @@
 package com.example.currentplacedetailsonmap;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 
+import android.os.StrictMode;
+//import android.util.JsonReader;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -39,7 +43,20 @@ import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
 import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
 import com.google.android.libraries.places.api.net.PlacesClient;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -74,14 +91,30 @@ public class MapsActivityCurrentPlace extends AppCompatActivity
     private static final String KEY_LOCATION = "location";
 
     // Used for selecting the current place.
-    private static final int M_MAX_ENTRIES = 5;
+    private static final int M_MAX_ENTRIES = 12;
     private String[] mLikelyPlaceNames;
     private String[] mLikelyPlaceAddresses;
     private List[] mLikelyPlaceAttributions;
     private LatLng[] mLikelyPlaceLatLngs;
 
+    private ArrayList<String> mList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        if (true) {
+//            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+//                    .detectDiskReads()
+//                    .detectDiskWrites()
+//                    .detectNetwork()   // or .detectAll() for all detectable problems
+//                    .penaltyLog()
+//                    .build());
+//            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+//                    .detectLeakedSqlLiteObjects()
+//                    .detectLeakedClosableObjects()
+//                    .penaltyLog()
+//                    .penaltyDeath()
+//                    .build());
+//        }
         super.onCreate(savedInstanceState);
 
         // Retrieve location and camera position from saved instance state.
@@ -97,6 +130,12 @@ public class MapsActivityCurrentPlace extends AppCompatActivity
         Places.initialize(getApplicationContext(), getString(R.string.google_maps_key));
         mPlacesClient = Places.createClient(this);
 
+        // Construct a GeoDataClient.
+        //mGeoDataClient = Places.getGeoDataClient(this, null);
+
+        // Construct a PlaceDetectionClient.
+        //mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
+
         // Construct a FusedLocationProviderClient.
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
@@ -104,6 +143,7 @@ public class MapsActivityCurrentPlace extends AppCompatActivity
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
 
     }
 
@@ -137,23 +177,10 @@ public class MapsActivityCurrentPlace extends AppCompatActivity
      */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Respond to menu item selection
-        switch (item.getItemId()) {
-            case R.id.option_get_place:
-                showCurrentPlace();
-                return true;
-            case R.id.nav_profile:
-                startActivity(new Intent(this, ProfileActivity.class));
-                return true;
-            case R.id.nav_leaderboard:
-                startActivity(new Intent(this, LeaderboardActivity.class));
-                return true;
-            case R.id.nav_share:
-                startActivity(new Intent(this, ShareActivity.class));
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.option_get_place) {
+            showCurrentPlace();
         }
+        return true;
     }
 
     /**
@@ -177,8 +204,7 @@ public class MapsActivityCurrentPlace extends AppCompatActivity
             @Override
             public View getInfoContents(Marker marker) {
                 // Inflate the layouts for the info window, title and snippet.
-                View infoWindow = getLayoutInflater().inflate(R.layout.custom_info_contents,
-                        (FrameLayout) findViewById(R.id.map), false);
+                View infoWindow = getLayoutInflater().inflate(R.layout.custom_info_contents, (FrameLayout) findViewById(R.id.map), false);
 
                 TextView title = infoWindow.findViewById(R.id.title);
                 title.setText(marker.getTitle());
@@ -211,6 +237,7 @@ public class MapsActivityCurrentPlace extends AppCompatActivity
         try {
             if (mLocationPermissionGranted) {
                 Task<Location> locationResult = mFusedLocationProviderClient.getLastLocation();
+                //String cloc =locationResult.toString();
                 locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
                     @Override
                     public void onComplete(@NonNull Task<Location> task) {
@@ -235,6 +262,7 @@ public class MapsActivityCurrentPlace extends AppCompatActivity
         } catch (SecurityException e)  {
             Log.e("Exception: %s", e.getMessage());
         }
+
     }
 
 
@@ -287,7 +315,9 @@ public class MapsActivityCurrentPlace extends AppCompatActivity
             return;
         }
 
-        if (mLocationPermissionGranted) {
+        if(mLocationPermissionGranted) {
+            getListOfRecyclePlants();
+
             // Use fields to define the data types to return.
             List<Place.Field> placeFields = Arrays.asList(Place.Field.NAME, Place.Field.ADDRESS,
                     Place.Field.LAT_LNG);
@@ -299,41 +329,35 @@ public class MapsActivityCurrentPlace extends AppCompatActivity
             // Get the likely places - that is, the businesses and other points of interest that
             // are the best match for the device's current location.
             @SuppressWarnings("MissingPermission") final
-            Task<FindCurrentPlaceResponse> placeResult =
-                    mPlacesClient.findCurrentPlace(request);
+            Task<FindCurrentPlaceResponse> placeResult = mPlacesClient.findCurrentPlace(request);
+
             placeResult.addOnCompleteListener (new OnCompleteListener<FindCurrentPlaceResponse>() {
                 @Override
                 public void onComplete(@NonNull Task<FindCurrentPlaceResponse> task) {
+                    //boolean is = task.isSuccessful();
+                    //boolean is = task.getResult()!=null;
+                    //System.out.println(isss);
                     if (task.isSuccessful() && task.getResult() != null) {
                         FindCurrentPlaceResponse likelyPlaces = task.getResult();
 
-                        // Set the count, handling cases where less than 5 entries are returned.
-                        int count;
-                        if (likelyPlaces.getPlaceLikelihoods().size() < M_MAX_ENTRIES) {
-                            count = likelyPlaces.getPlaceLikelihoods().size();
-                        } else {
-                            count = M_MAX_ENTRIES;
+
+                        List<List<String>> centers = parseArrayList();
+                        int sz = centers.get(0).size();
+                        mLikelyPlaceNames = new String[sz];
+                        mLikelyPlaceAddresses = new String[sz];
+                        mLikelyPlaceAttributions = new List[sz];
+                        mLikelyPlaceLatLngs = new LatLng[sz];
+                        for (int j = 0; j < sz ; j++) {
+                            mLikelyPlaceNames[j] = centers.get(0).get(j);//placeLikelihood.getPlace().getName();
+                            mLikelyPlaceAddresses[j] = centers.get(1).get(j);
+                            mLikelyPlaceLatLngs[j] = new LatLng(Double.parseDouble(centers.get(2).get(j)),Double.parseDouble(centers.get(3).get(j)));
                         }
 
-                        int i = 0;
-                        mLikelyPlaceNames = new String[count];
-                        mLikelyPlaceAddresses = new String[count];
-                        mLikelyPlaceAttributions = new List[count];
-                        mLikelyPlaceLatLngs = new LatLng[count];
 
-                        for (PlaceLikelihood placeLikelihood : likelyPlaces.getPlaceLikelihoods()) {
-                            // Build a list of likely places to show the user.
-                            mLikelyPlaceNames[i] = placeLikelihood.getPlace().getName();
-                            mLikelyPlaceAddresses[i] = placeLikelihood.getPlace().getAddress();
-                            mLikelyPlaceAttributions[i] = placeLikelihood.getPlace()
-                                    .getAttributions();
-                            mLikelyPlaceLatLngs[i] = placeLikelihood.getPlace().getLatLng();
-
-                            i++;
-                            if (i > (count - 1)) {
-                                break;
-                            }
-                        }
+                        //mLikelyPlaceAddresses[4] = placeLikelihood.getPlace().getAddress();
+                        //mLikelyPlaceAttributions[4] = placeLikelihood.getPlace()
+                        //        .getAttributions();
+                        //mLikelyPlaceLatLngs[4] = placeLikelihood.getPlace().getLatLng();
 
                         // Show a dialog offering the user the list of likely places, and add a
                         // marker at the selected place.
@@ -357,6 +381,128 @@ public class MapsActivityCurrentPlace extends AppCompatActivity
             // Prompt the user for permission.
             getLocationPermission();
         }
+    }
+    private List<List<String>> parseArrayList()
+    {
+        CharSequence name = "name";
+        CharSequence address = "formatted_address";
+        CharSequence loc = "location";
+        List<List<String>> rtr = new ArrayList<List<String>>();
+        ArrayList<String> n = new ArrayList<String>();
+        ArrayList<String> a = new ArrayList<String>();
+        ArrayList<String> la = new ArrayList<String>();
+        ArrayList<String> lo = new ArrayList<String>();
+
+        for (int i = 0; i < mList.size(); i++) {
+            String temp = mList.get(i);
+            //if (temp.startsWith("\"name\""))
+            //if(mList.get(i).contains(name))
+            if(temp.contains(name))
+            {
+                int nn = mList.get(i).indexOf(':');
+                n.add(mList.get(i).substring(nn+1));
+            }
+            if(temp.contains(address))
+            {
+                int an = mList.get(i).indexOf(':');
+                a.add(mList.get(i).substring(an+1));
+            }
+            if(temp.contains(loc))
+            {
+                int lt = mList.get(i+1).indexOf(':');
+                int lg = mList.get(i+2).indexOf(':');
+                la.add(mList.get(i+1).substring(lt+1,lt+9));
+                lo.add(mList.get(i+2).substring(lg+1,lg+9));
+            }
+        }
+        rtr.add(n);
+        rtr.add(a);
+        rtr.add(la);
+        rtr.add(lo);
+        return rtr;
+    }
+    private void getListOfRecyclePlants()
+    {
+        //ArrayList<String> rtr = null;
+        Thread t = new Thread(new Runnable() {
+            //@Override
+            public void run() {
+
+                URL url = null;
+
+                try {
+                    url = new URL(MapsActivityCurrentPlace.this.makeAPIUrl());
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    int h = urlConnection.getResponseCode();
+                    String here = "in here";
+                    String here2 = "no in here";
+
+                    try {
+                        InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                        //String out = in.toString();
+                        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                        //StringBuilder out = new StringBuilder();
+                        ArrayList<String> dat = new ArrayList<String>();
+                        int count = 0;
+                        String line;
+                        while ((line = br.readLine()) != null) {
+                            dat.add(line);
+                            //out.append(line);
+                        }
+                        mList = dat;
+                        //String jsonStr = out.toString();
+                        //System.out.println(jsonStr);   //Prints the string content read from input stream
+                        for (int i = 0; i < dat.size(); i++) {
+                            System.out.println(dat.get(i));
+                        }
+
+                        br.close();
+
+                        String here3 = "made it";
+                    } finally {
+                        urlConnection.disconnect();
+                        String hu = "didnt make it";
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });//.start();
+        t.setPriority(10);
+        t.start();
+        //return rtr;
+    }
+    private String makeAPIUrl()
+    {
+        String yourKey = "&key=AIzaSyB3BRZgLQdlEoDQY5tCbbKgrxbvizQIVsc";
+        String inputtype = "&inputtype=textquery";
+
+        String name = "input=recycle%20center";
+        double lat = mLastKnownLocation.getLatitude();
+        double lon = mLastKnownLocation.getLongitude();
+        String pos = String.valueOf(lat)+","+String.valueOf(lon);
+        String currentLoc = "location="+ String.valueOf(lat)+","+String.valueOf(lon);
+        //System.out.println(currentLoc);
+        String locBias = "&locationbias=circle:2000@"+pos;
+        String radius = "&radius=50000";
+        String keyword = "&keyword=recycle@20center";
+
+        String fields = "&fields=photos,formatted_address,name,rating,opening_hours,geometry";
+        String rankby = "&rankby=distance";
+
+        //text search
+        String query ="query=recycling+recycle+center+plant+near+me";
+        String base = "https://maps.googleapis.com/maps/api/place/textsearch/json?";
+        return base+query+currentLoc+radius+yourKey;
+
+        //nearbySearch
+        //String base = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+        //return base+currentLoc+radius+keyword+yourKey;
+
+        //find place
+        //String base = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?";
+        //return base+name+inputtype+fields+locBias+yourKey;
     }
 
     /**
